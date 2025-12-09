1711422a74686dc3a61fd51c2566adfe26781f2e
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-12-09 12:33:57 -0500

    crow can serve an image

---
+++ ReadME.md
@@ -0,0 +1,6 @@
+# Exploration of Building Crow microservice
+
+One interest I have with Crow is the potential speed of serving Binary data (images)
+for FindFirst. Currently the images are stored on a volume that is a physical mount
+stored on disk. If using crow can reduce the image retrieval time that would
+improve UX.
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,40 @@
 #include "crow.h"
-//#include "crow_all.h"
+#include <crow/http_response.h>
+#include <fstream>
+#include <ios>
 
-int main()
-{
-    crow::SimpleApp app; //define your crow application
+int main() {
+  crow::SimpleApp app; // define your crow application
 
-    //define your endpoint at the root directory
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world";
-    });
+  // define your endpoint at the root directory
+  CROW_ROUTE(app, "/")([]() {
+    crow::response res;
+    std::string fileName = "res/static/test_files/test.jpg";
+    std::stringstream imageBuffer;
 
-    //set the port, set the app to run on multiple threads, and run the app
-    app.port(18080).multithreaded().run();
+    // read example image
+    std::ifstream imageReq(fileName, std::ios_base::binary);
+    if (!imageReq) {
+      std::cout << "Error reading the image";
+      imageReq.close();
+      return crow::response(500, "Error getting image");
+    }
+
+    if (imageReq.is_open()) {
+      imageBuffer << imageReq.rdbuf();
+    }
+
+    std::uintmax_t filesize = std::filesystem::file_size(fileName);
+    // do some bound checking.
+    std::cout << filesize << std::endl;
+
+    std::string mime = "image/jpg";
+    res.set_header("Content-Type", mime);
+    res.body = imageBuffer.str();
+
+    return res;
+  });
+
+  // set the port, set the app to run on multiple threads, and run the app
+  app.port(18080).multithreaded().run();
 }
-

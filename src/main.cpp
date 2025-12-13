27d7bf9510928abb616a1df6746458eb3f228267
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2025-12-13 06:30:27 -0500

    dynamic envs and better error codes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "crow.h"
 #include <crow/http_response.h>
+#include <cstddef>
+#include <cstdlib>
 #include <fstream>
 #include <ios>
 #include <string>
@@ -12,13 +14,19 @@ int main() {
 
   // define your endpoint at the root directory
   CROW_ROUTE(app, "/<path>")([](std::filesystem::path image_name) {
-    std::string filename =
-        "res/static/test_files/" + image_name.filename().string();
+    std::string img_dir_path = "res/static/test_files/";
+    const char *img_dir = std::getenv("IMAGE_DIR");
+    // ensure that the env path exists before switching from safe default.
+    if (std::filesystem::exists(img_dir_path)) {
+      img_dir_path = std::string(img_dir);
+    }
+
+    std::string filename = img_dir_path + image_name.filename().string();
     std::string ext;
 
     // checks
     if (!std::filesystem::exists(filename)) {
-      return crow::response(500, "Image does not exist.");
+      return crow::response(404, "Image does not exist.");
     }
 
     if (image_name.has_extension()) {
@@ -27,12 +35,12 @@ int main() {
         return crow::response(500, "Images only supported");
       }
     } else {
-      return crow::response(500, "Images only supported");
+      return crow::response(400, "Images only supported");
     }
 
     std::uintmax_t filesize = std::filesystem::file_size(filename);
     if (filesize > MAX_SIZE) {
-      return crow::response(500, "Error getting image: image too large.");
+      return crow::response(400, "Error getting image: image too large.");
     }
 
     // read example image
